9837bc6fa34b547570c7bd9a9f3148fad7c6080e
Luka <51526367+LukaAhac@users.noreply.github.com>
2022-12-01 13:04:10 +0100

    adding TimeInterval model

---
+++ Models/TimeInterval.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class TimeInterval
+    {
+        public Guid Id { get; set; }
+        public string ProjectName { get; set; }
+        public DateTime TimeStart { get; set; } 
+        public DateTime TimeEnd { get; set; }
+
+    }
+}
