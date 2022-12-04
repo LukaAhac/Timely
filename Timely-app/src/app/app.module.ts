5bed1f3c2398fd3f0e76af830f79992c71d72aee
Luka <51526367+LukaAhac@users.noreply.github.com>
2022-12-05 00:57:03 +0100

    adding service for managing time intervals

--- Timely-app/src/app/app.module.ts
+++ Timely-app/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { TimeIntervalsGridComponent } from './time-intervals-grid/time-intervals
 import { DateTimeComponent } from './date-time/date-time.component';
 import { NameFormComponent } from './name-form/name-form.component';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -20,6 +21,7 @@ import { FormsModule,ReactiveFormsModule } from '@angular/forms';
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Timely-app/src/app/manage-intervals.service.ts
+++ Timely-app/src/app/manage-intervals.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { TimeInterval } from './models/timeInterval';
 
@@ -5,19 +6,65 @@ import { TimeInterval } from './models/timeInterval';
   providedIn: 'root'
 })
 export class ManageIntervalsService {
+  url: string = "http://localhost:5000/api";
+
   intervals: TimeInterval[] = [];
+  currentInterval: TimeInterval | null = null;
 
-
-  constructor() { }
+  
+  constructor(private http: HttpClient) {}
 
   getIntervals() : TimeInterval[] {
+    this.http.get(this.url + "/timeIntervals").subscribe((data : any) => {
+      data.forEach((element: any) => {
+        this.intervals.unshift({
+          id: element.id,
+          projectName: element.projectName,
+          timeStart: new Date(element.timeStart),
+          timeEnd: new Date(element.timeEnd),
+          duration: ""
+        });
+        this.intervals[0].duration = this.calculateDurationString(this.intervals[0])
+      });
+    });
     return this.intervals;
   }
 
-  addTimeInterval(interval: TimeInterval) : void {
+  startNewInterval() {
+    this.currentInterval = {
+      id: "testId",
+      projectName: ".....",
+      timeStart: new Date(),
+      timeEnd: null,
+      duration: "....."
+    };
+    this.addTimeInterval(this.currentInterval);
+  }
+
+  stopInterval(){
+    this.currentInterval!.timeEnd = new Date();
+    this.currentInterval!.duration = this.calculateDurationString(this.currentInterval!);
+  }
+
+  updateIntervalProjectName(projectName: string){
+    this.currentInterval!.projectName = projectName;
+  }
+
+  private addTimeInterval(interval: TimeInterval) : void {
     this.intervals.unshift(interval);
     if(this.intervals.length > 5){
       this.intervals.pop();
     }
+  }
+
+  private calculateDurationString(interval: TimeInterval) {
+    var duration = +(interval.timeEnd!) - +(interval.timeStart);
+    var seconds = Math.floor((duration / 1000) % 60);
+    var minutes = Math.floor((duration / (1000 * 60)) % 60);
+    var hours = Math.floor((duration / (1000 * 60 * 60)));
+
+    return (hours < 10 ? "0" + hours : hours) + ":" 
+          + (minutes < 10 ? "0" + minutes : minutes) + ":"
+          + (seconds < 10 ? "0" + seconds : seconds);
   }
 }
